9354aec74d9c55ca6cd32817113f0aef6813e043
hrushikeshahire1999 <hrushikeshahire1999@gmail.com>
2022-08-18 21:53:35 +0530

    added files

---
+++ test/src/test/kk.java
@@ -0,0 +1,18 @@
+package test;
+
+
+
+public class kk {
+    public void add(int a, int b)
+    {
+        System.out.println("Summ is :" + a+b);
+    }
+    public void add(int a, int b, int c)
+    {
+        System.out.println("Summ is :" + a+b+c);
+    }
+    public void multiply(int a, int b)
+    {
+        System.out.println("Product is :" + a*b);
+    }
+}
